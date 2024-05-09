1b6f4506976dfb640d053e4d969f305a66d56b35
austek <aliustek@hotmail.com>
2024-05-09 23:50:27 +0100

    Add expected expressions to test

--- src/test/java/com/cronutils/Issue605Test.java
+++ src/test/java/com/cronutils/Issue605Test.java
@@ -14,24 +14,25 @@ import java.util.stream.Stream;
 
 import static com.cronutils.model.CronType.QUARTZ;
 import static com.cronutils.model.CronType.SPRING;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class Issue605Test {
 
     static Stream<Arguments> cronExpressions() {
         return Stream.of(
-                Arguments.of(QUARTZ, CronMapper.fromQuartzToCron4j()),
-                Arguments.of(QUARTZ, CronMapper.fromQuartzToSpring()),
-                Arguments.of(QUARTZ, CronMapper.fromQuartzToUnix()),
-                Arguments.of(SPRING, CronMapper.fromSpringToQuartz())
+                Arguments.of(QUARTZ, CronMapper.fromQuartzToCron4j(), "0 0 * * 4#1"),
+                Arguments.of(QUARTZ, CronMapper.fromQuartzToSpring(), "0 0 0 ? * 4#1"),
+                Arguments.of(QUARTZ, CronMapper.fromQuartzToUnix(), "0 0 * * 4#1"),
+                Arguments.of(SPRING, CronMapper.fromSpringToQuartz(), "0 0 0 ? * 6#1 *")
         );
     }
 
     @ParameterizedTest
     @MethodSource("cronExpressions")
-    void testDayOfWeekMappingSpring(CronType cronType, CronMapper mapper) {
-        Cron cron = getCron(cronType, "0 0 0 ? * 5#1");
-        assertDoesNotThrow(() -> mapper.map(cron));
+    void testDayOfWeekMappingSpring(CronType cronType, CronMapper mapper, String expectedExpression) {
+        Cron sourceCron = getCron(cronType, "0 0 0 ? * 5#1");
+        String destinationCron = mapper.map(sourceCron).toString();
+        assertEquals(expectedExpression, destinationCron);
     }
 
     private Cron getCron(CronType cronType, @SuppressWarnings("SameParameterValue") final String quartzExpression) {
